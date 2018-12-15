cbb079b309d5a754cbbb994e65b9db2abd5b6ca0
Hubert Stärk <hubert.staerk@gmail.com>
2018-12-16 00:16:24 +0100

    add sass loader

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Page/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Page from './components/Page.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Page
   }
 }
 </script>
